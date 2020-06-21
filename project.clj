6717ded5751b3b88d6d91b28b93749321de2ca90
Abhinav Sharma <abhi18av@gmail.com>
2020-06-21 10:45:52 +0530

    tweak the config

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :profiles
     {:dev
-      {:dependencies [[org.clojure/clojure "1.10.1"]
-                      [org.clojure/clojurescript "1.10.773"]
+      {:dependencies [[org.clojure/clojurescript "1.10.773"]
                       [com.wsscode/pathom "2.2.31"]
                       [org.clojure/core.async "1.2.603"]
                       [appliedscience/js-interop "0.2.4"]
